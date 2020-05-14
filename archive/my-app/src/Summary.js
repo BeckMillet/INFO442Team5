45f01e3a7286c9ba68c45ada3d95ef804e21a612
Brian Maxwell <bmaxwell99@gmail.com>
2020-05-13 23:20:14 -0700

    updated the summary page

--- archive/my-app/src/Summary.js
+++ archive/my-app/src/Summary.js
@@ -76,11 +76,32 @@ export default class Summary extends Component {
     return button
   }
 
+  calcMonthName() {
+    let month = new Array(12);
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+    return month;
+  }
 
   render() {
     let overallBalance = Number(this.props.budgetToDate) + Number(this.props.dailyBudget) - Number(this.props.expensesToDate);
+
     let today = new Date();
-    today = today.toLocaleDateString()
+    let month = this.calcMonthName()
+    today = month[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear()
+
+    console.log(today)
 
     return (
       <div>
